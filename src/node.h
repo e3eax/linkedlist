20520d2652cef7d3c129f235915a726ca4005240
troublemanfx <143964705+troublemanfx@users.noreply.github.com>
2023-10-16 20:29:17 -0400

    Update node.h

--- src/node.h
+++ src/node.h
@@ -9,27 +9,27 @@ using std::endl;
 template <typename T>
 class Node {
  private:
-  int _key = 0;
-  T _data = 0;
-  Node* _next = nullptr;
+  int key_ = 0;
+  T data_ = 0;
+  Node* next_ = nullptr;
 
  public:
-  Node(T data = T()) : _data(data) {}
+  Node(T data = T()) : data_(data) {}
 
-  void set_next(Node* ptr) { _next = ptr; }
+  void set_next(Node* ptr) { next_ = ptr; }
 
-  void set_data(T data) { _data = data; }
+  void set_data(T data) { data_ = data; }
 
-  Node* next() const { return _next; }
+  Node* next() const { return next_; }
 
-  T data() const { return _data; }
+  T data() const { return data_; }
 
-  int key() const { return _key; }
+  int key() const { return key_; }
 
   void display() {
-    cout << "Key: " << _key << endl;
-    cout << "Data: " << _data << endl;
-    cout << "Next: " << _next << endl;
+    cout << "Key: " << key_ << endl;
+    cout << "Data: " << data_ << endl;
+    cout << "Next: " << next_ << endl;
   }
 };
 
